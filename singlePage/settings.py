9b5769ab8607a82c9ff7247cb2c870749071fdbc
Crazymind <crazymind2004@gmail.com>
2013-06-06 19:07:46 +0300

    pre-deploy changes

--- singlePage/settings.py
+++ singlePage/settings.py
@@ -59,15 +59,14 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join('cart/static/'),
+    #os.path.join('static/'),
     #'/'
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
